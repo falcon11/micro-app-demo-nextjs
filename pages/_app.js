648e1b4408139c95fbf8bf85d577524f8186e74e
Ashoka <savebtc11@gmail.com>
2020-12-23 10:38:47 +0800

    feat: 添加普通浏览器环境判断

--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,21 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     const initAlita = async () => {
       await import("alita-micro");
-      document.addEventListener("AlitaBridgeReady", () => {
+      // window.alitanative 存在：表示是 微应用 环境 或 微应用开发环境，此时要等待 WebViewJavascriptBridge 初始化完成
+      if (window.alitanative && !window.WebViewJavascriptBridge) {
+        document.addEventListener("AlitaBridgeReady", () => {
+          console.log('hello ready')
+          setIsAlitaMicroLoaded(true);
+          Plugins.forEach((plugin) => {
+            window.WebViewJavascriptBridge.registerPlugin(plugin);
+          });
+        });
+      } else {
         setIsAlitaMicroLoaded(true);
         Plugins.forEach((plugin) => {
-          window.WebViewJavascriptBridge.registerPlugin(plugin);
+          window?.WebViewJavascriptBridge?.registerPlugin(plugin);
         });
-      });
+      }
     };
     initAlita();
   }, []);
